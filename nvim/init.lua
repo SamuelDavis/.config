5959abef286d8a8170de19fd9af80d562ace3c23
Samuel Davis <samueljakdavis@gmail.com>
2023-05-03 10:00:05 -0400

    support for 'set'; support for plugins

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,7 +1,10 @@
+HOME_DIR = os.getenv("HOME")
+
 local function ensure_directory(path)
-    if not os.execute("cd " .. path) == 0 then
+    if os.execute("test -d '" .. path .. "'") ~= 0 then
         os.execute("mkdir -p " .. path)
     end
+    return path
 end
 
 local function apply_options(options)
@@ -10,15 +13,36 @@ local function apply_options(options)
     end
 end
 
-local colorscheme = "slate"
-local home_directory = os.getenv("HOME")
-local undo_directory = home_directory .. "/.vim/undos"
+local function apply_sets(options)
+    for _, value in pairs(options) do
+        vim.cmd("set " .. value)
+    end
+end
 
-ensure_directory(undo_directory)
+local function install_plugins(plugins)
+    local file = HOME_DIR .. "/.vim/autoload/plug.vim"
+    local url = "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
+    if os.execute("test -f '" .. file .. "'") ~= 0 then
+        print("Downloading Plug")
+        os.execute("curl -fLo " .. file .. " --create-dirs " .. url)
+    end
+
+    vim.call("plug#begin", HOME_DIR .. "/.vim/plugged")
+    for key, value in pairs(plugins) do
+        if value == false then
+            vim.fn["plug#"](key)
+        else
+            vim.fn["plug#"](key, config)
+        end
+    end
+    vim.call("plug#end")
+end
+
 apply_options({
     -- general
     wrap = false,
     colorcolumn = "80",
+    termguicolors = false,
     -- line number
     number = true,
     scrolloff = 10,
@@ -32,13 +56,23 @@ apply_options({
     swapfile = false,
     backup = false,
     undofile = true,
-    undodir = undo_directory,
+    undodir = ensure_directory(HOME_DIR .. "/.vim/undos"),
     -- search
     hlsearch = false,
     incsearch = true,
 })
 
-vim.cmd("colorscheme " .. colorscheme)
+apply_sets({
+    "autochdir",
+})
+
+vim.cmd("colorscheme slate")
+-- prevent the illusion that the cursor has jumped to the matching paren
+vim.cmd("hi MatchParen ctermfg=none ctermbg=none guifg=none guibg=none")
 -- restore transparency after setting colorscheme
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+
+install_plugins({
+    ["mbbill/undotree"] = false,
+})
